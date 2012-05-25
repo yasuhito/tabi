95ea071896da229f9d4f620818268d0233ecda1a
Yasuhito Takamiya <yasuhito@gmail.com>
2012-05-25 14:59:39 +0900

    空の rails アプリを追加

---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '18a6ba924a2ad301a319d7d36fafffb1955746239e0556960f081e9e0160391af7a112091bb8c68aac191c3edae14f91f8c3c75f93e98518b250f273c1fa01e5'
