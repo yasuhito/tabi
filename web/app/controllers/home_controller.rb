9dcdc9b25edf5d6a4ba14afc87a6d3b8c42d0277
Yasuhito Takamiya <yasuhito@gmail.com>
2012-05-26 08:24:41 +0900

    squid 経由なのでクライアントのアドレスを HTTP_X_FORWARDED_FOR から取るように修正

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
 
 
   def client_MAC
-    arp_output = `arp -n #{ request.env[ "REMOTE_ADDR" ] }`.split( "\n" )
+    arp_output = `arp -n #{ request.env[ "HTTP_X_FORWARDED_FOR" ] }`.split( "\n" )
     if arp_output.size == 2
       arp_output[ 1 ].split[ 2 ]
     else
