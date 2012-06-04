eb0f6fd59ad74fa02420a73374cbd847c2d57751
Yasuhito Takamiya <yasuhito@gmail.com>
2012-06-05 02:23:18 +0900

    クライアントの MAC アドレスを表示

--- web/app/controllers/welcome_controller.rb
+++ web/app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+  def index
+    arp_output = `arp -n #{ request.env[ "HTTP_X_FORWARDED_FOR" ] }`.split( "\n" )
+    if arp_output.size == 2
+      @client_mac = arp_output[ 1 ].split[ 2 ]
+    else
+      @client_mac = nil
+    end
+  end
 end
