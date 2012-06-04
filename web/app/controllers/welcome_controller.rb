6eb7423a81c9301f7cbfd315a7655e1ca01e74e8
Yasuhito Takamiya <yasuhito@gmail.com>
2012-06-05 02:40:23 +0900

    ログインできたら tabi コマンドで allow する

--- web/app/controllers/welcome_controller.rb
+++ web/app/controllers/welcome_controller.rb
@@ -1,10 +1,13 @@
+# -*- coding: utf-8 -*-
 class WelcomeController < ApplicationController
   def index
-    arp_output = `arp -n #{ request.env[ "HTTP_X_FORWARDED_FOR" ] }`.split( "\n" )
-    if arp_output.size == 2
-      @client_mac = arp_output[ 1 ].split[ 2 ]
-    else
-      @client_mac = nil
+    if user_signed_in?
+      arp_output = `arp -n #{ request.remote_ip }`.split( "\n" )
+      if arp_output.size == 2
+        user_mac = arp_output[ 1 ].split[ 2 ]
+        # TODO: ハードコードしているのをやめる
+        system "ssh yasuhito@192.168.0.254 /home/yasuhito/play/tabi/bin allow #{ user_mac }"
+      end
     end
   end
 end
