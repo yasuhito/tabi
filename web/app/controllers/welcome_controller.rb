5a7e325d074be8b84fe0cc4b22e701269ae4cd68
Yasuhito Takamiya <yasuhito@gmail.com>
2012-06-05 02:42:47 +0900

    tabi コマンドのパスがまちがっていたので修正

--- web/app/controllers/welcome_controller.rb
+++ web/app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
       if arp_output.size == 2
         user_mac = arp_output[ 1 ].split[ 2 ]
         # TODO: ハードコードしているのをやめる
-        system "ssh yasuhito@192.168.0.254 /home/yasuhito/play/tabi/bin allow #{ user_mac }"
+        system "ssh yasuhito@192.168.0.254 /home/yasuhito/play/tabi/bin/tabi allow #{ user_mac }"
       end
     end
   end
